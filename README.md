b37318b1a49ed1a9c431d5df4af2d247d12a047b
cyberabis <babishek@gmail.com>
2015-07-06 10:51:14 +0530

    kinesis integration for locations

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Stick API server
+
+Before starting set below environment variables:
+
+AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
--- routes/locations.js
+++ routes/locations.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+var kinesis = new AWS.Kinesis();
+var kinesis_stream = 'stick-locations';
 
 // middleware specific to this router
 router.use(function timeLog(req, res, next) {
@@ -22,9 +26,71 @@ module.exports = router;
 
 //Functions
 function send_to_kinesis(version, source_id, data) {
+	var parsed_data = parse_data(version, source_id, data);
+	var params = {
+	  Data: JSON.stringify(parsed_data), 
+	  PartitionKey: source_id,
+	  StreamName: kinesis_stream, 
+	};
+	kinesis.putRecord(params, function(err, data) {
+	  if (err) console.log(err, err.stack); // an error occurred
+	  else     console.log(data);           // successful response
+	});
+};
 
+//Parse data according to the version
+function parse_data(version, source_id, data) {
+	console.log('Raw data: ' + data + ', version: ' + version);
+	var parsed_data;
+	if(version === 'd1') {
+		data = data.substring(0, data.length - 1);
+		var events = data.split('$');
+		parsed_data = [];
+		for(var i in events) {
+			var fields = events[i].split(',');
+			var ts_input = fields[4];
+			var match = /^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2}).(\d{3})$/.exec(ts_input);
+			var ts = match[1] + '-' + match[2] + '-' + match[3] + ' ' + match[4] + ':' + match[5] + ':' + match[6] + '.' + match[7];
+			var ttff = parseInt(fields[5]);
+			var latlong = convert_location(parseFloat(fields[1]),parseFloat(fields[2]));
+			if (ttff > 0) {
+				var location_event = {
+					source_id: source_id,
+					lat: latlong[0],
+					long: latlong[1],
+					time: Date.parse(ts),
+					speed: parseFloat(fields[7])
+				};
+				parsed_data.push(location_event);
+			}
+		}
+		console.log('Parsed Data: ' + JSON.stringify(parsed_data));
+	} else
+		console.log('Source version not recognized');
+	return parsed_data;
+};
+
+//Utility used by parse_data
+function convert_location(latitude, longitude){
+	// convert latitude from minutes to decimal
+	degrees = Math.floor(latitude / 100);
+	minutes = latitude - (100 * degrees);
+	minutes /= 60;
+	degrees += minutes;
+	//turn direction into + or -
+	// if (latdir[0] == 'S') degrees *= -1;
+	lat = degrees;
+	//convert longitude from minutes to decimal
+	degrees = Math.floor(longitude / 100);
+	minutes = longitude - (100 * degrees);
+	minutes /= 60;
+	degrees += minutes;
+	//turn direction into + or -
+	//if (longdir[0] == 'W') degrees *= -1;
+	lon = degrees;
+	return [lat, lon];
 };
 
 function send_to_firebase(version, source_id, data) {
-
+	//TODO
 };

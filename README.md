6c78a085ed7efdfffc92d9b8552269129e14c543
cyberabis <babishek@gmail.com>
2015-07-06 11:53:36 +0530

    updates

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Stick API server
 
 Before starting set below environment variables:
 
-AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+STICK_API_PORT, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.STICK_API_PORT || 3000;
+
 //Routes
 var locations = require('./routes/locations');
 app.use('/api/locations', locations);
@@ -11,5 +13,5 @@ app.use('/api/events', events);
 var alerts = require('./routes/alerts');
 app.use('/api/alerts', alerts);
 
-app.listen(3000);
+app.listen(port);
 console.log('Stick App listening on port 3000');

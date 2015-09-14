03c4f5ffffeb9daee828461b71737e6f3812f5dd
koukumar <kousik.kumar@gmail.com>
2015-09-14 14:06:37 +0530

    loggly change

--- routes/assistnow.js
+++ routes/assistnow.js
@@ -3,16 +3,26 @@ var router = express.Router();
 var http = require('http');
 var fs = require('fs');
 
-router.get('/:lat/:lon', function(req, res) {
+var loggly = require('loggly');
+var loggly_token = process.env.LOGGLY_TOKEN || "7b9f6d3d-01ed-45c5-b4ed-e8d627764998";
+var loggly_sub_domain = process.env.LOGGLY_SUB_DOMAIN || "kousik"
+
+var client = loggly.createClient({
+    token: loggly_token,
+    subdomain: loggly_sub_domain,
+    tags: ["stick"],
+    json:true
+});
+
+router.get('/:lat/:lon/:version/:source_id', function(req, res) {
     var lat = req.params.lat;
     var lon = req.params.lon;
+    var source_id = req.params.source_id
 
-    console.log(token, gnss, datatype, lat, lon, pacc);
-    //url = 'http://online-live1.services.u-blox.com?token=' + token + ';gnss=' + gnss + ';datatype=' + datatype +
-        //';lat=' + lat + ';lon=' + lon + ';pacc=' + pacc;
     url = 'http://online-live1.services.u-blox.com/GetOnlineData.ashx?token=6NJ2C2z0iEyw9vth6Le_YA;' +
         'gnss=gps,qzss;datatype=eph,aux,pos;lat=' + lat + ';lon=' + lon + ';alt=0.000000;pacc=1000.000000';
     console.log(url);
+    client.log(data, ["debug", "assistnow", source_id]);
     var buffer = null;
     var request = http.get(url, function(response) {
         if (response.statusCode === 200) {

26525fe86d3ec61f540079e7dc112e559e565075
koukumar <kousik.kumar@gmail.com>
2017-04-06 18:40:04 +0530

    debug logs

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    console.log({ "DEBUGS" : "----BIG-ISSUE----", "url" : req.url, "body" : req.body,  "method" : req.method, "params" : req.params})
     client.log({ "url" : req.url, "body" : req.body,  "method" : req.method, "params" : req.params});
     next();
 });
